26b94193c7ca34916ac3e1e81463dc5e32ab8f78
Michael <michaelkindye@gmail.com>
2025-08-25 18:07:35 +0300

    added tags for messages framework

--- DjMessage/settings.py
+++ DjMessage/settings.py
@@ -51,6 +51,16 @@ CHANNEL_LAYERS = {
     }
 }
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'error'
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
